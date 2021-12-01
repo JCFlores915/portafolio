440441ffc08558089d3dc7e2fac28306e208a577
Juan Carlos Flores Gutierrez <juanflores915@yahoo.es>
2021-12-01 17:10:54 -0600

    primer commit

---
+++ precache-manifest.c212339e1aaf2a00526ecfcaf4fafe0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a54d2161639859d40e94a23b42c1b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1d51efa2d27b70e632",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5890f334c4db5305ee3c",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "ae1d51efa2d27b70e632",
+    "url": "/static/js/2.a958bd77.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a958bd77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5890f334c4db5305ee3c",
+    "url": "/static/js/main.ee88712d.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
